0502c92015065eda2557b314b197ffd4d84ee0cc
shiba <hdswick@gmail.com>
2019-01-20 15:12:16 -0600

    Styling added. Working like version 0

--- invoice-generator.scm
+++ invoice-generator.scm
@@ -3,39 +3,50 @@
 (use-modules (sxml simple))
 
 (define (personal-info)
-  `(div
-    (div ,(assoc-ref data "name"))
+  `(div (@ (style "display: inline-block; width: 80%;"))
+    (h3 ,(assoc-ref data "name"))
     (div ,(assoc-ref data "address1"))
     (div ,(assoc-ref data "address2"))
     (div ,(string-append "Phone: " (assoc-ref data "phone")))))
 
 (define (invoice-info)
-  `(div
-    (div "INVOICE")
+  `(div (@ (style "display: inline-block;"))
+    (h1 "INVOICE")
     (div ,(string-append "INVOICE #" (assoc-ref data "invoice-number")))
 
     (div ,(assoc-ref data "current-date"))))
 
 (define (client-info)
-  `(div
+  `(div (@ (style "padding-top: 100px"))
+    (h3 "To:")
     (div ,(assoc-ref data "client-name"))
     (div ,(assoc-ref data "client-address1"))
     (div ,(assoc-ref data "client-address2"))))
 
 (define (payment-info)
-  `(div
-    (div "Payment by Wire Transfer to:")
+  `(div (@ (style "padding-top: 100px;"))
+    (h3 "Payment by Wire Transfer to:")
     (div ,(string-append "Bank Name " (assoc-ref data "bank-name")))
     (div ,(string-append "Bank Routing Number " (assoc-ref data "routing-number")))
     (div ,(string-append "Account Number " (assoc-ref data "account-number")))))
 
+(define (table-styling)
+  `(style "table, th, td { border: 1px solid black; border-collapse: collapse; }"))
+
 (define (invoice-table)
   `(div
-    (div "Invoice Item")
-    (div "Invoice Amount")
-    (div ,(assoc-ref data "invoice-item"))
-    (div ,(assoc-ref data "invoice-amount"))
-    (div ,(assoc-ref data "total-amount"))))
+    (@ (style "padding-top: 100px;"))
+    (table
+     (@ (style "width: 70%;"))
+     (tr
+      (th "Item")
+      (th "Amount"))
+     (tr
+      (td ,(assoc-ref data "invoice-item"))
+      (td ,(assoc-ref data "invoice-amount")))
+     (tr
+      (td (b "Total:"))
+      (td ,(assoc-ref data "total-amount"))))))
 
 (define (invoice-html)
   `(html
@@ -43,7 +54,9 @@
     (body
      ,(personal-info)
      ,(invoice-info)
+     ,(client-info)
      ,(payment-info)
+     ,(table-styling)
      ,(invoice-table))))
 
 (call-with-output-file "temp.html"

94b318d03561d628ee0140a9ee11a027d2fbcd14
shiba <hdswick@gmail.com>
2019-01-22 23:13:40 -0600

    Input data is more modular now

--- README.md
+++ README.md
@@ -10,28 +10,52 @@ apt install guile wkhtmltopdf
 
 # Usage
 
-Create `data.scm` with this content:
+Create `personal.scm` like this, it can be reused for other invoices:
 ```
-(define data
+(define personal
   '(("name" . "Shiba Inu")
     ("address1" . "420 Shibenobi Rd")
     ("address2" . "Dark Side, Moon 55555")
     ("phone" . "555-555-5555")
-    ("client-name" . "Client Name")
-    ("client-address1" . "4343 Goobar Dr.")
-    ("client-address2" . "CA 56732")
     ("bank-name" . "Chase")
     ("routing-number" . "3467543")
-    ("account-number" . "123456789")
-    
-    ;invoice specific
-    ("invoice-number" . "2")
-    ("current-date" . "JANUARY 1, 2019")
-    ("invoice-item" . "December 1-31 2018")
-    ("invoice-amount" . "$200.00")
-    ("total-amount" . "$200.00")))
+    ("account-number" . "123456789")))    
 ```
+
+Make a directory example: `data`.
+
+```bash
+mkdir data
+touch data/client.scm
+touch data/invoice.scm
+```
+
+Then fill client in like this:
+
+```
+(define client
+  '(("client-name" . "Annoying Client")
+    ("client-address1" . "420 Cherry Dr, San Francisco")
+    ("client-address2" . "CA 55555")))
+```
+
+Last but not least, fill in invoice like this:
+```
+(define (item date amount)
+  (cons date amount))
+
+(define items
+  (list (item "January 1-15 2019" 1500.00)))
+
+(define invoice-meta-data
+  '(("invoice-number" . "1")
+    ("current-date" . "1/16/2019")))
+```
+
+Now we can cleanly run the script:
 
 ```bash
 guile -s invoice-generator.scm
 ```
+
+You should see a file named `output.pdf` now.
--- invoice-generator.scm
+++ invoice-generator.scm
@@ -1,37 +1,51 @@
-(load "data.scm")
+(define base "data/")
+
+(load "personal.scm")
+(load (string-append base "invoice.scm"))
+(load (string-append base "client.scm"))
 
 (use-modules (sxml simple))
 
 (define (personal-info)
   `(div (@ (style "display: inline-block; width: 80%;"))
-    (h3 ,(assoc-ref data "name"))
-    (div ,(assoc-ref data "address1"))
-    (div ,(assoc-ref data "address2"))
-    (div ,(string-append "Phone: " (assoc-ref data "phone")))))
+    (h3 ,(assoc-ref personal "name"))
+    (div ,(assoc-ref personal "address1"))
+    (div ,(assoc-ref personal "address2"))
+    (div ,(string-append "Phone: " (assoc-ref personal "phone")))))
 
 (define (invoice-info)
   `(div (@ (style "display: inline-block;"))
     (h1 "INVOICE")
-    (div ,(string-append "INVOICE #" (assoc-ref data "invoice-number")))
-
-    (div ,(assoc-ref data "current-date"))))
+    (div ,(string-append "INVOICE #" (assoc-ref invoice-meta-data "invoice-number")))
+    (div ,(assoc-ref invoice-info "current-date"))))
 
 (define (client-info)
   `(div (@ (style "padding-top: 100px"))
     (h3 "To:")
-    (div ,(assoc-ref data "client-name"))
-    (div ,(assoc-ref data "client-address1"))
-    (div ,(assoc-ref data "client-address2"))))
+    (div ,(assoc-ref client "client-name"))
+    (div ,(assoc-ref client "client-address1"))
+    (div ,(assoc-ref client "client-address2"))))
 
 (define (payment-info)
   `(div (@ (style "padding-top: 100px;"))
     (h3 "Payment by Wire Transfer to:")
-    (div ,(string-append "Bank Name " (assoc-ref data "bank-name")))
-    (div ,(string-append "Bank Routing Number " (assoc-ref data "routing-number")))
-    (div ,(string-append "Account Number " (assoc-ref data "account-number")))))
+    (div ,(string-append "Bank Name: " (assoc-ref personal "bank-name")))
+    (div ,(string-append "Bank Routing Number: " (assoc-ref personal "routing-number")))
+    (div ,(string-append "Account Number: " (assoc-ref personal "account-number")))))
 
 (define (table-styling)
   `(style "table, th, td { border: 1px solid black; border-collapse: collapse; }"))
+
+(define (invoice-items)
+  (map
+   (lambda (item)
+     `(tr
+       (td ,(car item))
+       (td ,(cdr item))))
+   items))
+
+(define (calc-total)
+  (apply + (map cdr items)))
 
 (define (invoice-table)
   `(div
@@ -40,13 +54,11 @@
      (@ (style "width: 70%;"))
      (tr
       (th "Item")
-      (th "Amount"))
-     (tr
-      (td ,(assoc-ref data "invoice-item"))
-      (td ,(assoc-ref data "invoice-amount")))
+      (th "Amount ($)"))
+     ,(invoice-items)
      (tr
       (td (b "Total:"))
-      (td ,(assoc-ref data "total-amount"))))))
+      (td ,(calc-total))))))
 
 (define (invoice-html)
   `(html
@@ -66,5 +78,5 @@
     (sxml->xml (invoice-html) port)
     (newline port)))
 
-(system "wkhtmltopdf temp.html test.pdf")
+(system "wkhtmltopdf temp.html output.pdf")
 (system "rm -f temp.html")
